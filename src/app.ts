f66f5593fe8067af2125ce2c4b2771512fc609d0
SOVEREIGN <isimemeni@gmail.com>
2021-07-06 12:47:24 -0400

    working on drag events

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// Drag & Drop Interfaces
+interface  Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandlerevent(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dropLeaveHandler(event: DragEvent): void;
+}
+
+
 // Project Type
 enum ProjectStatus {Active, Finished}
 
@@ -138,7 +151,8 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement>{
 }
 
 //Project Item Class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> 
+    implements Draggable {
     private project: Project;
 
     public get persons() : string {
@@ -157,7 +171,16 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent();
     }
 
-    configure(){};
+    @autobind
+    dragStartHandler(event: DragEvent){}
+
+    dragEndHandler(_: DragEvent){}
+
+    configure(){
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+
+    };
 
     renderContent(){
         this.element.querySelector('h2')!.textContent = this.project.title;
