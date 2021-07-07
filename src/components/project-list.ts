857567797c89d415f74d74160662d77d1957900f
SOVEREIGN <isimemeni@gmail.com>
2021-07-07 10:10:01 -0400

    adding webpack to project

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { DragTarget } from "../models/drag-drop.js";
-import { autobind } from "../decorators/autobind.js"; 
-import { Component } from "./base-component.js";
-import { projectState } from "../state/project-state.js"; 
-import { Project, ProjectStatus } from "../models/project.js";
-import { ProjectItem } from "./project-item.js";
+import { DragTarget } from "../models/drag-drop";
+import { autobind } from "../decorators/autobind"; 
+import { Component } from "./base-component";
+import { projectState } from "../state/project-state"; 
+import { Project, ProjectStatus } from "../models/project";
+import { ProjectItem } from "./project-item";
 
  //Project List Class
     export class ProjectList extends Component<HTMLDivElement, HTMLElement> 
